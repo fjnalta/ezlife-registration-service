4e833e29f57d490bb436c3fd0d07e241f37f8b90
ajo <fjnalta@gmail.com>
2020-04-19 01:24:05 +0200

    update iRedMail Script

--- lib/tools/create_mail_user_OpenLDAP.sh
+++ lib/tools/create_mail_user_OpenLDAP.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-# Author:   Zhang Huangbin (zhb _at_ iredmail.org)
+# Author:   Philipp Minges
 # Purpose:  Add new OpenLDAP user for postfix mail server.
-# Project:  iRedMail (http://www.iredmail.org/)
-
-# --------------------------- WARNING ------------------------------
-# This script only works under iRedMail >= 0.8.4 due to ldap schema
-# changes.
-# ------------------------------------------------------------------
+# Description: Original iRedMail addUser Script modded for ezlife.eu
 
 # --------------------------- USAGE --------------------------------
-# 1) Please change variables below to fit your env:
+# 1) Change variables below to fit your env:
 #
 #   - In 'Global Setting' section:
 #       * STORAGE_BASE_DIRECTORY
@@ -21,30 +16,6 @@
 #       * BINDPW
 #       * QUOTA
 #
-#   - In 'Virtual Domains & Users' section:
-#       * QUOTA
-#       * TRANSPORT
-#       * PASSWORD_SCHEME       # SSHA is recommended.
-#       * DEFAULT_PASSWD
-#       * USE_DEFAULT_PASSWD
-#
-#   - Pure-FTPd integration:
-#       * PUREFTPD_INTEGRATION      # <- set to 'YES' if you want to integrate it.
-#       * FTP_BASE_DIRECTORY        # <- directory used to store FTP data.
-#
-#   - Optional variables:
-#       * SEND_WELCOME_MSG
-#
-# 2) Execute this script with domain name and username (without @domain) directly:
-#
-#       shell# bash create_mail_user_OpenLDAP.sh example.com new_user
-#
-#    It will create a mail user with mail address "new_user@example.com".
-#    To add multiple mail users, just list all usernames:
-#
-#       shell# bash create_mail_user_OpenLDAP.sh example.com new_user new_user2 new_user3
-#
-# ------------------------------------------------------------------
 
 # Source functions.
 . global
@@ -129,59 +100,13 @@ STORAGE_NODE="$(basename ${STORAGE_BASE_DIRECTORY})"
 EPOCH_SECONDS="$(date +%s)"
 DAYS_SINCE_EPOCH="$((EPOCH_SECONDS / 24 / 60 / 60))"
 
-add_new_domain()
-{
-    domain="$(echo ${1} | tr '[A-Z]' '[a-z]')"
-    ldapsearch -x -D "${BINDDN}" -w "${BINDPW}" -b "${BASE_DN}" | grep "domainName: ${domain}" >/dev/null
-
-    if [ X"$?" != X"0" ]; then
-        echo "Add new domain: ${domain}."
-
-        ldapadd -x -D "${BINDDN}" -w "${BINDPW}" <<EOF
-dn: ${DOMAIN_DN},${BASE_DN}
-objectClass: mailDomain
-domainName: ${domain}
-mtaTransport: ${TRANSPORT}
-accountStatus: active
-enabledService: mail
-EOF
-    else
-        :
-    fi
-
-    ldapadd -x -D "${BINDDN}" -w "${BINDPW}" <<EOF
-dn: ${OU_USER_DN},${DOMAIN_DN},${BASE_DN}
-objectClass: organizationalUnit
-objectClass: top
-ou: Users
-EOF
-
-    ldapadd -x -D "${BINDDN}" -w "${BINDPW}" <<EOF
-dn: ou=Groups,${DOMAIN_DN},${BASE_DN}
-objectClass: organizationalUnit
-objectClass: top
-ou: Groups
-EOF
-
-    ldapadd -x -D "${BINDDN}" -w "${BINDPW}" <<EOF
-dn: ou=Aliases,${DOMAIN_DN},${BASE_DN}
-objectClass: organizationalUnit
-objectClass: top
-ou: Aliases
-EOF
-
-    ldapadd -x -D "${BINDDN}" -w "${BINDPW}" <<EOF
-dn: ou=Externals,${DOMAIN_DN},${BASE_DN}
-objectClass: organizationalUnit
-objectClass: top
-ou: Externals
-EOF
-}
-
 add_new_user()
 {
     USERNAME="$(echo $1 | tr [A-Z] [a-z])"
     MAIL="$( echo $2 | tr [A-Z] [a-z])"
+    UID="$( echo $3 | tr [A-Z] [a-z])"
+    CN="$( echo $4 | tr [A-Z] [a-z])"
+    SN="$( echo $5 | tr [A-Z] [a-z])"
 
     maildir="${DOMAIN_NAME}/$(hash_maildir ${USERNAME})"
 
@@ -221,10 +146,10 @@ mailMessageStore: ${STORAGE_NODE}/${maildir}
 mail: ${MAIL}
 mailQuota: ${QUOTA}
 userPassword: ${PASSWD}
-cn: ${USERNAME}
-sn: ${USERNAME}
+cn: ${CN}
+sn: ${SN}
 givenName: ${USERNAME}
-uid: ${USERNAME}
+uid: ${UID}
 shadowLastChange: ${DAYS_SINCE_EPOCH}
 amavisLocal: TRUE
 enabledService: internal
@@ -271,29 +196,26 @@ send_welcome_mail()
 usage()
 {
     echo "Usage:"
-    echo -e "\t$0 DOMAIN USERNAME"
-    echo -e "\t$0 DOMAIN USER1 USER2 USER3 ..."
+    echo -e "\t$0 DOMAIN USERNAME UID CN SN"
 }
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 5 ]; then
     usage
 else
     # Promopt to check settings.
     [ X"${LDAP_SUFFIX}" == X"dc=example,dc=com" ] && echo "You should change 'LDAP_SUFFIX' in $0."
 
-    # Get domain name.
+    # Get Parameters.
     DOMAIN_NAME="$(echo $1 | tr '[A-Z]' '[a-z]')"
-    shift 1
+    USERNAME="$(echo $2 | tr [A-Z] [a-z])"
+    MAIL="${USERNAME}@${DOMAIN_NAME}"
+    UID="$( echo $3 | tr [A-Z] [a-z])"
+    CN="$( echo $4 | tr [A-Z] [a-z])"
+    SN="$( echo $5 | tr [A-Z] [a-z])"
 
-    add_new_domain ${DOMAIN_NAME}
-    for i in $@; do
-        USERNAME="$(echo $i | tr '[A-Z]' '[a-z]')"
-        MAIL="${USERNAME}@${DOMAIN_NAME}"
+    # Add new user in LDAP.
+    add_new_user ${USERNAME} ${MAIL} ${UID} ${CN} ${SN}
 
-        # Add new user in LDAP.
-        add_new_user ${USERNAME} ${MAIL}
-
-        # Send welcome msg to new user.
-        [ X"${SEND_WELCOME_MSG}" == X'YES' ] && send_welcome_mail ${MAIL}
-    done
+    # Send welcome msg to new user.
+    [ X"${SEND_WELCOME_MSG}" == X'YES' ] && send_welcome_mail ${MAIL}
 fi
