d893782885e8ac28fdc8ea0e556b1d2e1e00dc49
ajo <fjnalta@gmail.com>
2020-04-19 02:00:38 +0200

    update script call

--- lib/tools/create_User.sh
+++ lib/tools/create_User.sh
@@ -4,5 +4,5 @@ TOOLSDIR=$(pwd)/lib/tools
 SCRIPT=create_mail_user_OpenLDAP.sh
 
 sed -i "s/ezlife-password/$2/g" $TOOLSDIR/$SCRIPT
-$TOOLSDIR/$SCRIPT ezlife.eu $1 $3 $4
+bash $TOOLSDIR/$SCRIPT ezlife.eu $1 $3 $4
 sed -i "s/$2/ezlife-password/g" $TOOLSDIR/$SCRIPT
