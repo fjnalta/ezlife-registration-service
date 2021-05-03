326943116680c0c2430ec1412524900886d53fff
fjnalta <fjnalta@gmail.com>
2021-05-03 22:06:52 +0200

    reload page on successful account deletion

--- public/js/handleAccountSettings.js
+++ public/js/handleAccountSettings.js
@@ -60,6 +60,7 @@ function deleteAccount() {
                     $("#accountDeleteStatus").text("Account wurde erfolgreich gelöscht");
                     $("#accountDeleteStatus").css("font-weight","Bold");
                     $("#accountDeleteStatus").css('color', 'green');
+                    location.reload();
                 },
                 error: function () {
                     $("#accountDeleteStatus").text("Accountlöschung fehlgeschlagen");
